068abbd41f846ac9ac1f92ed41560773f5995b06
can.tas <can.tas@bilgeadam.com>
2024-10-24 23:57:23 +0300

    ✨ create and apply loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "./../features/cart/CartOverview";
+import Loader from "./Loader";
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <main>{isLoading ? <Loader /> : <Outlet />}</main>
       <CartOverview />
     </div>
   );
