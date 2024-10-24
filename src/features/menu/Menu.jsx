242657c91b9a4173850b4968d393ccf4e7bcc575
can.tas <can.tas@bilgeadam.com>
2024-10-24 21:53:06 +0300

    👽️ implement data loading with react router

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 const Menu = () => {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu?.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+};
+
+export const loader = async () => {
+  const response = await getMenu();
+  return response;
 };
 
 export default Menu;
