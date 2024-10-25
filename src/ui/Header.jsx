86ce34c2a984061d5dadc50fb4190df515d3e4c8
can.tas <can.tas@bilgeadam.com>
2024-10-25 14:17:22 +0300

    🌱 add search input for orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
-      <p>Fırat Can Taş</p>
+      <SearchOrder />
+      <h4>Fırat Can Taş</h4>
     </header>
   );
 };
